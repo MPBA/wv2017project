3df94ee3bcd7d17c405c05cd10947fe0e54e766c
elliot <elliot.gorokhovsky@gmail.com>
2017-06-28 21:54:48 +0200

    Finalized buy_entry_credits

--- pyfactom.py
+++ pyfactom.py
@@ -3,56 +3,56 @@ import json, requests, uuid
 DAEMON_URL = 'http://13.90.96.240:8088/v2'
 WALLET_URL = 'http://13.90.96.240:8089/v2'
 
-class json_rpc:
-    def __init__(self):
-        self.counter = -1
+FACTOID_ADDRESS = 'FA3bNrrt7F34ANPDnRdQESRhN3SD3MQJqUZWdpbKGX2J66wxBFbP'
+EC_ADDRESS = 'EC1rV8ZrsscKmTA2K4CX3xQRPki2FM18E79MRoJuiAq9g4yGoFhB'
 
-    def encode(self, method, params=''):
-        self.counter += 1
-        return json.dumps({
-            'jsonrpc': '2.0',
-            'id': self.counter,
-            'method': method,
-            'params': params
-        })
 
-    def decode(self, message):
-        return json.loads(message)['result']
+def encode(method, params=''):
+    return json.dumps({
+        'jsonrpc': '2.0',
+        'id': uuid.uuid4().hex[:30],
+        'method': method,
+        'params': params
+    })
 
-RPC = json_rpc()
+def decode(message):
+    return json.loads(message)['result']
+
 
 def make_wallets():
     '''Creates factoid and entry credit wallets on the server and returns a tuple of the addresses.'''
     
     factoid_request = requests.post(
         url=WALLET_URL,
-        data=RPC.encode('generate-factoid-address'),
+        data=encode('generate-factoid-address'),
     )
 
     ec_request =  requests.post(
         url=WALLET_URL,
-        data=RPC.encode('generate-ec-address'),
+        data=encode('generate-ec-address'),
     )
 
-    return (RPC.decode(factoid_request.text)['public'],
-            RPC.decode(ec_request.text)['public'])
+    return (decode(factoid_request.text)['public'],
+            decode(ec_request.text)['public'])
 
 def buy_entry_credits(factoid_address, ec_address, amount=None):
     '''
     Deposits entry credits in ec_address using funds from factoid_address. 
     If amount is None, use all the funds, else use amount * 10e8 Factoids.
-    If factoid_address does not have enough factoids, IOError will be raised.
     '''
 
+    fee = 10**5
+    
     # Get the balance of factoid_address (in Factoshis = Factoids * 10e8) if amount is None
     if amount == None:
-        balance_request = requests.push(
+        balance_request = requests.post(
             url=DAEMON_URL,
-            data=RPC.encode('factoid-balance', {'address': factoid_address})
+            data=encode('factoid-balance', {'address': factoid_address})
         )
-        balance = RPC.decode(balance_request.text)['balance']
+        balance = decode(balance_request.text)['balance']
+        amount = max(balance - fee, 0)
     else:
-        balance = amount
+        amount = max(amount - fee, 0)
         
     # Get a unique transaction name
     TX_NAME = uuid.uuid4().hex[:30]
@@ -60,58 +60,51 @@ def buy_entry_credits(factoid_address, ec_address, amount=None):
     # Start a new transaction
     res = requests.post(
         url=WALLET_URL,
-        data=RPC.encode('new-transaction', {'tx-name': TX_NAME})
+        data=encode('new-transaction', {'tx-name': TX_NAME})
     )
 
     # Add all funds from our factoid address as an input
     res = requests.post(
         url=WALLET_URL,
-        data=RPC.encode('add-input', {'tx-name': TX_NAME,
+        data=encode('add-input', {'tx-name': TX_NAME,
                                       'address': factoid_address,
-                                      'amount': balance})
+                                      'amount': amount})
     )
 
     # Route the entire input to the entry credit output
     res = requests.post(
         url=WALLET_URL,
-        data=RPC.encode('add-ec-output', {'tx-name': TX_NAME,
+        data=encode('add-ec-output', {'tx-name': TX_NAME,
                                           'address': ec_address,
-                                          'amount': balance})
+                                          'amount': amount})
     )
 
     # Specify that the transaction fee should be subtracted from the entry credit output
     res = requests.post(
         url=WALLET_URL,
-        data=RPC.encode('sub-fee', {'tx-name': TX_NAME,
-                                    'address': ec_address})
+        data=encode('add-fee', {'tx-name': TX_NAME,
+                                    'address': factoid_address})
     )
 
     # Sign the transaction
     res = requests.post(
         url=WALLET_URL,
-        data=RPC.encode('sign-transaction', {'tx-name': TX_NAME})
+        data=encode('sign-transaction', {'tx-name': TX_NAME})
     )
 
     # Get the hex for the transaction
     compose_request = requests.post(
         url=WALLET_URL,
-        data=RPC.encode('compose-transaction', {'tx-name': TX_NAME})
+        data=encode('compose-transaction', {'tx-name': TX_NAME})
     )
 
-    transaction_hex = RPC.decode(compose_request.text)['params']['transaction']
+    transaction_hex = decode(compose_request.text)['params']['transaction']
 
     # Submit the transaction to the network using factoid-submit
     submit_request = requests.post(
         url=DAEMON_URL,
-        data=RPC.encode('factoid-submit', {'transaction': transaction_hex})
+        data=encode('factoid-submit', {'transaction': transaction_hex})
     )
-    response = RPC.decode(submit_request.text)
-    print(response)
-    if response != 'Successfully submitted the transaction':
-        raise IOError('Transaction submission failed!')
+    response = decode(submit_request.text)['message']
 
-    
-buy_entry_credits('FA3bNrrt7F34ANPDnRdQESRhN3SD3MQJqUZWdpbKGX2J66wxBFbP',
-                  'EC1rV8ZrsscKmTA2K4CX3xQRPki2FM18E79MRoJuiAq9g4yGoFhB',
-                  amount=100000)
- 
+buy_entry_credits(FACTOID_ADDRESS, EC_ADDRESS, amount=100000)

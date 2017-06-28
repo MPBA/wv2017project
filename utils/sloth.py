7641bdb2e1ec015d517a71ca370fd6459c3d998e
Andrea Zanin <zanin_andrea@outlook.com>
2017-06-28 22:03:58 +0200

    fixed bug with sloth utils

--- utils/sloth.py
+++ utils/sloth.py
@@ -7,19 +7,20 @@ from os import listdir
 
 def loadSloth(imagesDir, annotationPos):
     # loading the annotations
+    fileNames = listdir(imagesDir)
     file = open(annotationPos, "r")
     loaded=json.load(file)
     d={}
     for i in loaded:
         d[i["filename"]]=i["annotations"]
-
+    
     # populating X and y
     X=[]
     y=[]
     for imageName in fileNames:
         X.append(ndimage.imread(imagesDir+imageName))
         y.append(d[imageName])
-    return X,y
+    return X,y,fileNames
 
 def saveSloth(fileNames, y):
     annotations=[]

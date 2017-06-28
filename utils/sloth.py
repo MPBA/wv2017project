fa8aea63c42787eea6f3de69364f001ddd558337
Andrea Zanin <zanin_andrea@outlook.com>
2017-06-28 21:50:21 +0200

    added sloth utils

---
+++ utils/sloth.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy import ndimage
+import matplotlib.pyplot as plt
+from matplotlib import patches
+import json
+from os import listdir
+
+def loadSloth(imagesDir, annotationPos):
+    # loading the annotations
+    file = open(annotationPos, "r")
+    loaded=json.load(file)
+    d={}
+    for i in loaded:
+        d[i["filename"]]=i["annotations"]
+
+    # populating X and y
+    X=[]
+    y=[]
+    for imageName in fileNames:
+        X.append(ndimage.imread(imagesDir+imageName))
+        y.append(d[imageName])
+    return X,y
+
+def saveSloth(fileNames, y):
+    annotations=[]
+    for idx, imageName in enumerate(fileNames):
+        entry={
+            "annotations": y[idx],
+            "class": "image",
+            "filename": imageName
+        }
+        annotations.append(entry)
+    slothFile=json.dumps(annotations)
+    return slothFile

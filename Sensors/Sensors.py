d89986d34d5ae23fdaeb369f3aaec1909bf80995
jkm99zambo <marcozamboni2013107@gmail.com>
2017-07-03 10:04:30 +0200

    Generalized the function to a class so can be used with multiple sensors

--- Sensors/Sensors.py
+++ Sensors/Sensors.py
@@ -3,37 +3,43 @@
 import GPIO
 import time
 
-Trigger=18
-Echo=24
 
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-def takeDistance(trust=False,numIteration=1):
-    distance=[]
-    for i in numIteration:
-        GPIO.output(GPIO_TRIGGER, True)
-        time.sleep(0.00001)
-        GPIO.output(GPIO_TRIGGER, False)
-        StartTime = time.time()
-        StopTime = time.time()
-        while GPIO.input(GPIO_ECHO) == 0:
+
+class SR04:
+    initialized=[]
+    def initialize(TRIG,ECHO):
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.setup(ECHO, GPIO.IN)
+        self.initialized.append(TRIG)
+    def takeDistance(self,TRIG,ECHO,trust=False,numIteration=1):
+        if not TRIG in self.initialized:
+            self.initialize(TRIG,ECHO)
+        distance=[]
+        for i in numIteration:
+            GPIO.output(TRIG, True)
+            time.sleep(0.00001)
+            GPIO.output(TRIG, False)
             StartTime = time.time()
-        while GPIO.input(GPIO_ECHO) == 1:
             StopTime = time.time()
-        TimeElapsed = StopTime - StartTime
-        distance.append((TimeElapsed * 34300) / 2)
-    medDistance=0
-    for i in distance:
-        medDistance+=i
-    medDistance/=len(distance)
-    if trust:
+            while GPIO.input(ECHO) == 0:
+                StartTime = time.time()
+            while GPIO.input(ECHO) == 1:
+                StopTime = time.time()
+            TimeElapsed = StopTime - StartTime
+            distance.append((TimeElapsed * 34300) / 2)
+        medDistance=0
         for i in distance:
-            if abs(medDistance-i)/medDistance > 0.1:
-                return False
-    return medDistance
+            medDistance+=i
+        medDistance/=len(distance)
+        if trust:
+            for i in distance:
+                if abs(medDistance-i)/medDistance > 0.1:
+                    return False
+        return medDistance
 
 
 
 if __name__='__main__':
-    distance=takeDistance(2)
+    dist=SR04()
+    dist.takeDistance(18,19)

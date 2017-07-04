ebfb2609fc6ff2597b3646baf09a87efd1a3c8aa
elliot <elliot.gorokhovsky@gmail.com>
2017-07-04 11:56:43 +0200

    Actual

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='chainEN',
+    name='pynotarize',
     version='1.0',
-    description='Notarizes files on the bitcoin blockchain.',
-    author='Elliot and Nunzio',
-    author_email='elliot.gorokhovsky@gmail.com',
-    url='https://xxxxxx',
-    data_files=[('config', 'chainEN/config.json')],
-    packages=['chainEN'],
-    install_requires=['redis']
+    packages=['pynotarize']
 )
